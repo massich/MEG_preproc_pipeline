b6d1b55d8b28da6b3b47a054bd364e9fed55a88b
Joan Massich <sik@visor.udg.edu>
2019-01-30 15:38:14 +0100

    add a paragraph describing what 00-fetch-data does

--- 00-fetch_data.py
+++ 00-fetch_data.py
@@ -3,6 +3,10 @@
 00. Fetch MNE-Python sample data
 ================================
 
+This script makes use of mne sample data to drive the scripts. It downloads the
+sample data and stores it at mne default directory.
+
+Replace this script with something that downloads and stores your data if needed.
 """  # noqa: E501
 
 
